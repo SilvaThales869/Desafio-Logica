9fcf42e84153b17352e8377f30fcfce386bc51d5
SilvaThales869 <silvathales869@gmail.com>
2023-11-07 10:39:05 -0300

    desafio finalisado

--- Desafio_classificador_de_niveis.js
+++ Desafio_classificador_de_niveis.js
@@ -1,1 +1,19 @@
-console.log ("ohio sekai")
+var XP = 523
+
+const hero = "O herói é nível: "
+if(XP<=1000)
+console.log(hero+"Ferro")
+else if(XP>=1001&XP<=2000)
+console.log(hero+"Bronze")
+else if(XP>=2001&XP<=5000)
+console.log(hero+"Prata")
+else if(XP>=5001&XP<=7000)
+console.log(hero+"Ouro")
+else if(XP>=7001&XP<=8000)
+console.log(hero+"Platina")
+else if(XP>=8001&XP<=9000)
+console.log(hero+"Ascendente")
+else if(XP>=9001&XP<=10000)
+console.log(hero+"Imortal")
+else
+console.log(hero+"Radiante")
---
+++ Teste.js
@@ -0,0 +1,6 @@
+var XP = 523
+var GoblinCount = 0
+var RunCount = 0
+var nome = console.prompt("What is your name?")
+
+
